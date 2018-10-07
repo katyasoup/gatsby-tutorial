b4b5e1a044121b1599476fbd4dbfe8d074604a14
katyasoup <campbell.katharine@gmail.com>
2018-10-07 14:38:01 -0500

    tutorial part two (typography.js) and reorganize directory structure

---
+++ tutorial-part-two/src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+// import bootstrapTheme from "typography-theme-bootstrap";
+import moragaTheme from "typography-theme-moraga"
+
+const typography = new Typography(moragaTheme)
+
+export default typography
